7a82ea48138d1e7acf93e3c7c4ed319f82b06f87
cramsel <Luxicorde@gmail.com>
2022-01-19 17:57:01 -0600

    Add working routes to make the full CRUD api for products. Does NOT allow for new images or a new supplier at this time.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,38 @@ class ProductsController < ApplicationController
   def index
     render json: Product.all
   end
+
+  def show
+    product = Product.find_by(id: params[:id])
+    render json: product
+  end
+
+  def create
+    product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      supplier_id: params[:supplier_id],
+      quantity: params[:quantity],
+    )
+    render json: product
+    #validators make an if statement go here
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.description = params[:description] || product.description
+    product.supplier_id = params[:supplier_id] || product.supplier_id
+    product.quantity = params[:quantity] || product.quantity
+    #validators make the render an if statement "if product.save..."
+    render json: product
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.delete
+    render json: "Product Deleted"
+  end
 end
