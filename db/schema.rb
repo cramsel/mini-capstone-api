6cbd07fb934772d6d87cea019f36b10bcc824e29
cramsel <Luxicorde@gmail.com>
2022-01-18 21:07:40 -0600

    Add working controllers, as well as index pathway for HTTP requests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_022059) do
+ActiveRecord::Schema.define(version: 2022_01_19_024918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2022_01_19_022059) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.string "description"
+    t.text "description"
     t.integer "supplier_id"
     t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
