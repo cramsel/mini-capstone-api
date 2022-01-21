e7e4ca316ea3f9403da3b947cc40f489b74da1ec
cramsel <Luxicorde@gmail.com>
2022-01-20 19:10:01 -0600

    Add working POST route for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ApplicationRecord
+  validates :user_id, presence: true
+  validates :product_id, presence: true
+  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than: 0 }
+  validates :subtotal, presence: true
+  validates :tax, presence: true
+  validates :total, presence: true
 
   belongs_to :user
   belongs_to :product
