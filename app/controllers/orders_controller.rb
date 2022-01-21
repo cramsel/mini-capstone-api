9a3ca37f5d9e6ba92851935b867d566c0e2073fa
cramsel <Luxicorde@gmail.com>
2022-01-20 20:59:17 -0600

    Clean the Show and Index routes in the order controller by adding variables and using built-in rails methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,29 @@
 class OrdersController < ApplicationController
   def index
-    order_list = Order.where(user_id: current_user.id)
+    #order_list = Order.where(user_id: current_user.id)
+    order_list = current_user.orders
     render json: order_list
   end
 
   def show
-    order = Order.find_by(id: params[:id])
+    #order = Order.find_by(id: params[:id])
+    order = current_user.orders.find_by(id: params[:id])
     render json: order
   end
 
   def create
     product = Product.find_by(id: params[:product_id])
+    quantity = params[:quantity]
+    subtotal = product.price * quantity
+    tax = product.tax * quantity
+
     order = Order.create(
       user_id: current_user.id,
       product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: params[:quantity] * product.price,
-      tax: product.tax * params[:quantity],
-      total: (product.tax * params[:quantity]) + (product.price * params[:quantity]),
+      quantity: quantity,
+      subtotal: subtotal,
+      tax: product.tax * quantity,
+      total: tax + subtotal,
     )
 
     if order.save
