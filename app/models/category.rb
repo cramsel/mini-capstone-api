951273c62f2fb89bc6f463dddf651464fd65ac30
cramsel <Luxicorde@gmail.com>
2022-01-24 19:17:59 -0600

    Add working products method within Category class, which displays the product information based on the category_products table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,12 @@
 class Category < ApplicationRecord
   has_many :category_products
+  # has_many :products
+
+  def products
+    items = []
+    category_products.each do |n|
+      items << Product.find_by(id: n[:product_id])
+    end
+    items
+  end
 end
